dcec432991b209fedb468af4767becba008b5c97
Erik LaBianca <erik.labianca@gmail.com>
2018-03-09 11:25:31 -0500

    Update to jackson 2.8 and latest AHC1

--- src/main/java/com/ning/billing/recurly/model/jackson/RecurlyObjectsSerializer.java
+++ src/main/java/com/ning/billing/recurly/model/jackson/RecurlyObjectsSerializer.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 
 import javax.xml.namespace.QName;
 
+import com.fasterxml.jackson.core.json.JsonWriteContext;
+import com.fasterxml.jackson.dataformat.xml.deser.XmlTokenStream;
 import com.ning.billing.recurly.model.RecurlyObject;
 import com.ning.billing.recurly.model.RecurlyObjects;
 
@@ -48,7 +50,7 @@ public class RecurlyObjectsSerializer<T extends RecurlyObjects<U>, U extends Rec
 
         final ToXmlGenerator xmlgen = (ToXmlGenerator) jgen;
         // Nested RecurlyObjects
-        xmlgen.getOutputContext().writeFieldName(elementName);
+        ((JsonWriteContext)xmlgen.getOutputContext()).writeFieldName(elementName);
         boolean firstValue = true;
         for (final U value : values) {
             if (firstValue) {
